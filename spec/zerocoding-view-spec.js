33705da39e341e79a9745428b4a4d73c9908fbc9
D14na <support@d14na.org>
2018-08-04 15:58:38 -0400

    First commit.

---
+++ spec/zerocoding-view-spec.js
@@ -0,0 +1,9 @@
+'use babel'
+
+import ZeroCodingView from '../lib/zerocoding-view'
+
+describe('ZeroCodingView', () => {
+    it('has one valid test', () => {
+        expect('life').toBe('easy')
+    })
+})
